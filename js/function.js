4437d25c4f8717ca06909cd81bc9fe63ef049d41
Алексей Бороденко <phoenixx8483@gmail.com>
2024-06-26 23:55:58 +0300

    1

--- js/function.js
+++ js/function.js
@@ -1,30 +1,22 @@
-//    1. Функция для проверки длины строки.
-function checkLength (str, length) {   //Создаем функцию checkLength с параметрами str и length
-  if (str.length <= length) {  // условие - длина параметра str <= lenght
+// 1. Функция для проверки длины строки.
+function checkLength (str, length) {
+  if (str.length <= length) {
    console.log(true);
    } else {
      console.log(false);
    }
-  };
- checkLength ('Привет, Денис!!!', 50);   //Вызываем функцию с заданными аргументами
-
-
-
- //    2.  Функция для проверки, является ли строка палиндромом.
- function checkPalindrom (string) {   //создаем функцию с параметром string
-  let normalString = string.toLocaleLowerCase().replaceAll(' ', ''); //убираем пробелы и приводим к нижнему регистру
-  let newString = '';
-
-  for (let i = normalString.length - 1; i >= 0; i--) {  //начало циклы = длина строки - 1 (c конца строки)
-
-  newString += normalString[i]; //получаем и записываем символ с конца строки normalString в начало строки newString
   }
-
-  if (newString === normalString) {  //проверка на строгое равенство
-      console.log('Ура, \"' + string + '"\ является палиндромом!!!')
-      }  else {
-          console.log('\"' + string + '"\ не палиндром. Попробуйте ещё.')
+ checkLength ('Привет, Денис!!!', 50);
+ // 2.  Функция для проверки, является ли строка палиндромом.
+ function checkPalindrom (string) {
+ const normalString = string.toLocaleLowerCase().replaceAll(' ', '');
+  for (let i = normalString.length - 1; i >= 0; i--) {
+  newString += normalString[i];
+  }
+  if (newString === normalString) {
+      console.log('Ура, ' + string + ' является палиндромом!!!');
+      } else {
+          console.log(string + 'не палиндром. Попробуйте ещё.');
       }
-
-  };
+  }
   checkPalindrom('т опоТ ');
