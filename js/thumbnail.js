12c56f2d04a4d7bc235c1d0744f049860fb78ebf
Алексей Бороденко <phoenixx8483@gmail.com>
2024-07-29 14:10:56 +0300

    Отрисуй меня полностью

---
+++ js/thumbnail.js
@@ -0,0 +1,24 @@
+const thumbnailTemplate = document.querySelector('#picture').content;
+const container = document.querySelector('.pictures');
+
+const createThumbnail = (picture) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = picture.url;
+  thumbnail.querySelector('.picture__img').alt = picture.description;
+  thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+  thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
+  return thumbnail;
+};
+
+const generateThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+  container.append(fragment);
+};
+
+export{generateThumbnails};
+
+
