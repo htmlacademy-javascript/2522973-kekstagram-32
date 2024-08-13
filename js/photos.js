b2bc06b0e51f4683dc8577ae380892ed0ddb02ef
Алексей Бороденко <phoenixx8483@gmail.com>
2024-08-13 07:33:51 +0300

    Перламутровые пуговицы

--- js/photos.js
+++ js/photos.js
@@ -3,21 +3,25 @@ import { showBigPicture } from './big-photo.js';
 
 const container = document.querySelector('.pictures');
 
-const renderPhotos = (pictures) => {
-  container.addEventListener('click', (evt) => {
-    const thumbnail = evt.target.closest('[data-thumbnail-id]');
-    if (!thumbnail) {
-      return;
-    }
+let pictures = [];
 
-    evt.preventDefault();
-    const picture = pictures.find(
-      (item) => item.id === +thumbnail.dataset.thumbnailId
-    );
-    showBigPicture(picture);
-  });
+const onContainerClick = (evt) => {
+  const thumbnail = evt.target.closest('[data-thumbnail-id]');
+  if (!thumbnail) {
+    return;
+  }
 
+  evt.preventDefault();
+  const picture = pictures.find(
+    (item) => item.id === +thumbnail.dataset.thumbnailId
+  );
+  showBigPicture(picture);
+};
+
+const renderPhotos = (currentPictures) => {
+  pictures = currentPictures;
   generateThumbnails(pictures, container);
+  container.addEventListener('click', onContainerClick);
 };
 
 export { renderPhotos };
